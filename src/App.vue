15c5e392fadc802c92aa60e8c897a6397b988ea0
Anderson Balbino <andersonbalbino2212@outlook.com>
2024-02-15 20:58:09 -0300

    Reorganizando o projeto Criando a pasta componente e separando a calculadora em uma pasta única e importando no App.vue, atualizando variáveis e a função de calcular

--- src/App.vue
+++ src/App.vue
@@ -11,42 +11,9 @@
 • Crie um repositório no Github para esta tarefa e nos envie o link pela plataforma.  -->
 
 
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      numero1: 0,
-      numero2: 0,
-      operacao: 'somar',
-      resultado: 0,
-    };
-  },
+<script setup>
+import Calculadora from './components/Calculadora.vue'
 
-  watch: {
-    operacao: 'calcularResultado',
-  },
-  methods: {
-    calcularResultado() {
-      switch (this.operacao) {
-        case 'somar':
-          this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
-          break;
-        case 'subtrair':
-          this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
-          break;
-        case 'dividir':
-          this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
-          break;
-        case 'multiplicar':
-          this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
-          break;
-        default:
-          break;
-      }
-    },
-  }
-}
 </script>
 
 <template>
@@ -54,27 +21,6 @@ export default {
     <header class="p-5 mb-4 mt-4 bg-light rounded-3">
       <h1>Calculadora Aritmética</h1>
     </header>
-    <form class="bg-light p-4">
-      <div class="row">
-        <div class="col">
-          <input v-model="numero1" class="mb-5" type="number" />
-          <input v-model="numero2" type="number" />
-        </div>
-        <div class="col">
-          <p>Selecione uma operação</p>
-          <select v-model="operacao" class="form-control">
-            <option value="somar">Somar</option>
-            <option value="dividir">Dividir</option>
-            <option value="subtrair">Subtrair</option>
-            <option value="multiplicar">Multiplicar</option>
-          </select>
-        </div>
-        <div class="col">
-          <p>O resultado é: <span class="fs-4">{{ resultado }}</span></p>
-        </div>
-      </div>
-    </form>
+    <Calculadora />
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+import { reactive } from 'vue';
+
+const estado = reactive({
+    numero1: 0,
+    numero2: 0,
+    operacao: 'somar',
+    resultado: 0,
+
+});
+
+ 
+ const calcularResultado = () => {
+      switch (estado.operacao) {
+        case 'somar':
+          estado.resultado = parseFloat(estado.numero1) + parseFloat(estado.numero2);
+          break;
+        case 'subtrair':
+          estado.resultado = parseFloat(estado.numero1) - parseFloat(estado.numero2);
+          break;
+        case 'dividir':
+          estado.resultado = parseFloat(estado.numero1) / parseFloat(estado.numero2);
+          break;
+        case 'multiplicar':
+          estado.resultado = parseFloat(estado.numero1) * parseFloat(estado.numero2);
+          break;
+        default:
+          break;
+      }
+ }
+</script>
+
+<template>
+     <form class="bg-light p-4">
+      <div class="row">
+        <div class="col">
+          <input v-model="estado.numero1" class="mb-5" type="number" @keyup="calcularResultado()" @change="calcularResultado()" />
+          <input v-model="estado.numero2" type="number"  @keyup="calcularResultado()" @change="calcularResultado()" />
+        </div>
+        <div class="col">
+          <p>Selecione uma operação</p>
+          <select v-model="estado.operacao" class="form-control" @change="calcularResultado()">
+            <option value="somar">Somar</option>
+            <option value="dividir">Dividir</option>
+            <option value="subtrair">Subtrair</option>
+            <option value="multiplicar">Multiplicar</option>
+          </select>
+        </div>
+        <div class="col">
+          <p>O resultado é: <span class="fs-4">{{ estado.resultado }}</span></p>
+        </div>
+      </div>
+    </form>
+</template>
